cfb7acd8a058a1ab1dbcc242c1571cc4e87c4107
Daniil10Sh <danik2022odessa@gmail.com>
2024-03-10 18:17:39 +0200

    add

---
+++ script.js
@@ -0,0 +1,19 @@
+
+let num = 0;
+while (num < 10) {
+    num += 1;
+console.log(num)
+}
+
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i * 2);
+}
+
+
+const array = [ '1', '2', '3', '4', '5',]
+while (array < 5) {
+    array += 1;
+console.log(array)
+}
+
